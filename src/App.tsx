3d750a37f37c3799c046754c8cd57c51a0f0ffca
edward <edwardlfish@gmail.com>
2025-12-02 23:26:59 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css';
+import {useState} from "react";
+import Task from "./components/Task.tsx";
+
+const App = () => {
+    const [tasks, setTasks] = useState<string[]>([]);
+
+    const deleteTask = (index: number) => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    }
+
+    const ediTask = (index: number, text: string) => {
+        const newTasks = [...tasks];
+        newTasks[index] = text;
+        setTasks(newTasks);
+    }
+
+    const addTask = () => {
+        setTasks([...tasks, 'New task']);
+    }
+
+    return (
+        <div className={'field'}>
+            <button onClick={addTask} className={'btn new'}>Add task</button>
+            {tasks.map((t, i) => <Task key={i + 1} index={i} remove={deleteTask} edit={ediTask}>{t}</Task>)}
+        </div>
+    )
+}
+
+export default App
